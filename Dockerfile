74ea492557ea2ecb91f288d36d5c6d7cd8166e41
refazul <refazul.refat@yahoo.com>
2023-11-08 21:37:32 +0600

    include ma_gym folder

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY marllib/ marllib/
 CMD ["python", "./marllib/patch/add_patch.py", "-y"]
-
 RUN pip install marllib
+
+COPY ma_gym/ ma_gym/
 RUN pip install ma-gym
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 from marllib import marl
 # prepare env
 env = marl.make_env(environment_name="mpe", map_name="simple_spread")
@@ -7,3 +8,16 @@ mappo = marl.algos.mappo(hyperparam_source="mpe")
 model = marl.build_model(env, mappo, {"core_arch": "mlp", "encode_layer": "128-256"})
 # start training
 mappo.fit(env, model, stop={"timesteps_total": 1000000}, checkpoint_freq=100, share_policy="group", num_gpus=0)
+'''
+import gym
+
+env = gym.make('ma_gym:Switch2-v0')
+done_n = [False for _ in range(env.n_agents)]
+ep_reward = 0
+
+obs_n = env.reset()
+while not all(done_n):
+    #env.render()
+    obs_n, reward_n, done_n, info = env.step(env.action_space.sample())
+    ep_reward += sum(reward_n)
+env.close()
